7ea4bea8035e663d642875929988db920bf1d035
miraz <105309402+Miraz661@users.noreply.github.com>
2024-03-07 13:01:13 +0600

    Update index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+const form = document.querySelector(".content");
+
+form.addEventListener("submit",(e)=>{
+    let Fname = e.target.fName.value;
+    let Lname = e.target.lName.value;
+    let email = e.target.email.value;
+    let num = e.target.phone.value;
+    let addr = e.target.address.value;
+    
+})
